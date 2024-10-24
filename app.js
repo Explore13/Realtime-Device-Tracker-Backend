4145a670bf21c6617dbbab77442ca7106c159b99
Explore13 <suryaghoshchemistry2k21@gmail.com>
2024-10-25 03:00:03 +0530

    Username feature added

--- app.js
+++ app.js
@@ -3,8 +3,6 @@ const app = express();
 import http from "http";
 import { fileURLToPath } from "url";
 import path, { dirname } from "path";
-import dotenv from "dotenv";
-dotenv.config();
 
 import { Server } from "socket.io";
 
@@ -12,20 +10,20 @@ const server = http.createServer(app);
 const io = new Server(server);
 const __filename = fileURLToPath(import.meta.url);
 const __dirname = dirname(__filename);
-const PORT = process.env.PORT;
 
 app.set("view engine", "ejs");
 app.use(express.static(path.join(__dirname, "public")));
 
 io.on("connection", (socket) => {
   socket.on("send-location", (position) => {
-    console.log(position.latitude, position.longitude);
-    io.emit("receive-location", { id: socket.id, ...position });
+    const { latitude, longitude, userName } = position;
+    console.log(`User: ${userName}, Latitude: ${latitude}, Longitude: ${longitude}`);
+    io.emit("receive-location", { id: socket.id, latitude, longitude, userName });
   });
+
   socket.on("disconnect", () => {
     io.emit("user-disconnected", socket.id);
-    console.log(socket.id +" Disconnected");
-    
+    console.log(socket.id + " disconnected");
   });
 
   console.log(socket.id + " Connected");
@@ -35,4 +33,6 @@ app.get("/", (req, res) => {
   res.render("index");
 });
 
-server.listen(PORT || 3000);
+server.listen(3000, () => {
+  console.log('Server running on port 3000');
+});
--- public/js/script.js
+++ public/js/script.js
@@ -1,12 +1,14 @@
 const socket = io();
 
+const userName = prompt("Enter your name (Leave to show 'Unknown') : ");
+
 if (navigator.geolocation) {
   navigator.geolocation.watchPosition(
     (position) => {
       const { latitude, longitude } = position.coords;
       console.log(latitude, longitude);
 
-      socket.emit("send-location", { latitude, longitude });
+      socket.emit("send-location", { latitude, longitude, deviceName });
     },
     (error) => {
       console.error(error);
@@ -27,13 +29,17 @@ L.tileLayer("https://{s}.tile.openstreetmap.org/{z}/{x}/{y}.png", {
 const markers = {};
 
 socket.on("receive-location", (data) => {
-  const { id, latitude, longitude } = data;
+  const { id, latitude, longitude, deviceName } = data;
+
   map.setView([latitude, longitude]);
   if (markers[id]) {
     markers[id].setLatLng([latitude, longitude]);
   } else {
     markers[id] = L.marker([latitude, longitude]).addTo(map);
+    markers[id].bindPopup(`<b>User: ${userName || "Unknown"}</b>`);
   }
+
+  markers[id].openPopup();
 });
 
 socket.on("user-disconnected", (id) => {
